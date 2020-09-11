cdd09f31917e6caeaa58652247dd30cba0f4e478
Brad Harris <bmharris@gmail.com>
2020-09-11 16:12:48 -0600

    empty app state for frontiers demo starting point

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,13 +1,10 @@
 import { App } from "@slack/bolt";
-import { configureRandomMemberStep } from "./random-member-step";
 
 export const configureBoltApp = (): App => {
   const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
   });
-
-  configureRandomMemberStep(app);
 
   return app;
 };
